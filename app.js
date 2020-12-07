9760468ba3c55273bc39e22f237864fba9c4df53
asiefmahir <asiefmahir@gmail.com>
2020-12-08 04:00:55 +0600

    removed the console logs

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const sentencesCount = document.getElementById("sentence-count");
  * Bind the functions to EventListener
  */
 /**
- * /**
  * Step 3
  * Find the data and variable
  */
@@ -28,6 +27,57 @@ const sentencesCount = document.getElementById("sentence-count");
  * Step 6
  * Compose two functions in another function
  */
+
+/**
+ * Step 7
+ * Refactor the code
+ */
+
+// let word = 0;
+// let sentence = 0;
+// function wordCount() {
+// 	let words = textArea.value.split(/[.|!|?|' ']+/g);
+// 	let filteredWords = [];
+// 	for (let i = 0; i < words.length; i++) {
+// 		let w = words[i];
+// 		let counter = 0;
+
+// 		for (let k = 0; k < w.length; k++) {
+// 			if (
+// 				(w[k] >= "A" && w[k] <= "Z") ||
+// 				(w[k] >= "a" && w[k] <= "z") ||
+// 				(w[k] >= "0" && w[k] <= "9")
+// 			) {
+// 				counter++;
+// 			}
+// 		}
+
+// 		if (counter !== 0) {
+// 			filteredWords.push(words[i]);
+// 		}
+
+// 		word = filteredWords.length;
+// 		wordsCount.textContent = word;
+// 	}
+// }
+
+// function sentenceCount() {
+// 	let arr = textArea.value.replace(/\s/g, "").split(/[.|!|?]+/g);
+// 	let sentences = [];
+// 	for (let i = 0; i < arr.length; i++) {
+// 		if (!arr[i].includes(" ")) {
+// 			sentences.push(arr[i]);
+// 		}
+// 	}
+// 	sentence = sentences.length - 1;
+// 	sentencesCount.textContent = sentence;
+// }
+// function count() {
+// 	wordCount();
+// 	sentenceCount();
+// }
+
+// textArea.addEventListener("input", count);
 
 function isAlphaNumeric(char) {
 	return (
@@ -66,10 +116,8 @@ function getSentenceCount(sentences) {
 	let count = 0;
 	let test = [];
 	sentencesArray.forEach((s) => {
-		console.log(sentencesArray);
 		if (isValidWord(s)) {
 			test.push(s);
-			console.log(test);
 			count++;
 		}
 	});
